4bfb21df098ac92de94df84917a063c1aac0d901
paujorbaa <pau.jorbaa@gmail.com>
2025-09-16 11:40:45 +0200

    single enemy, path working

--- main.js
+++ main.js
@@ -10,10 +10,22 @@ const path = [
   { x: 7, y: 3 },
   { x: 7, y: 5 },
   { x: 1, y: 5 },
-  { x: 1, y: 6 },
-  { x: 9, y: 6 },
-  { x: 9, y: 8 },
+  { x: 1, y: 7 },
+  { x: 4, y: 7 },
+  { x: 4, y: 8 },
 ];
+const enemyTypes = {
+  small: { hp: 10, speed: 90, lives: 1, class: "enemy small" },
+  medium: { hp: 30, speed: 60, lives: 5, class: "enemy medium" },
+  quick: { hp: 20, speed: 120, lives: 3, class: "enemy quick" },
+  boss: { hp: 120, speed: 35, lives: 999, class: "enemy boss" },
+};
+
+const enemies = [];
+
+let lastTime = performance.now();
+
+// GRID
 
 function buildGrid(cols = COLS, rows = ROWS) {
   game.style.setProperty("--cols", cols);
@@ -34,9 +46,45 @@ function buildGrid(cols = COLS, rows = ROWS) {
 
 buildGrid();
 
-// ENEMY
+// PATH
 
-let lastTime = performance.now();
+function markPathTiles() {
+  for (let i = 0; i < path.length - 1; i++) {
+    const a = path[i];
+    const b = path[i + 1];
+
+    if (a.y === b.y) {
+      const startX = Math.min(a.x, b.x);
+      const endX = Math.max(a.x, b.x);
+      for (let x = startX; x <= endX; x++) {
+        if (x >= 0 && a.y >= 0 && x < COLS && a.y < ROWS) {
+          const selector = '.tile[data-x="' + x + '"][data-y="' + a.y + '"]';
+          const tile = game.querySelector(selector);
+          if (tile) {
+            tile.classList.add("path");
+          }
+        }
+      }
+    }
+
+    if (a.x === b.x) {
+      const startY = Math.min(a.y, b.y);
+      const endY = Math.max(a.y, b.y);
+      for (let y = startY; y <= endY; y++) {
+        if (a.x >= 0 && y >= 0 && a.x < COLS && y < ROWS) {
+          const selector = '.tile[data-x="' + a.x + '"][data-y="' + y + '"]';
+          const tile = game.querySelector(selector);
+          if (tile) {
+            tile.classList.add("path");
+          }
+        }
+      }
+    }
+  }
+}
+markPathTiles();
+
+// ENEMY
 
 function gameLoop(timestamp) {
   const delta = (timestamp - lastTime) / 1000;
@@ -73,3 +121,11 @@ enemy.x = path[0].x * 60;
 enemy.y = path[0].y * 60;
 enemy.targetIndex = 1;
 enemy.speed = 90;
+
+/* 
+mark path tiles 
+movement function --> new function, run while i < enemies[] 
+
+
+
+*/
